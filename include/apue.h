0e52323867e2c31df3e17a4a47d4aada71f0b505
BrilliantLJW <name321@qq.com>
2018-03-31 21:59:38 +0800

    update

--- include/apue.h
+++ include/apue.h
@@ -20,7 +20,7 @@
 
 #define DIR_MODE (FILE_MODE|S_IXUSR|S_IXGRP|S_IXOTH)
 
-typedef void Sigfunc(init);
+typedef void Sigfunc(int);
 
 #define min(a,b) ((a)<(b)?(a):(b))
 #define max(a,b) ((a)>(b)?(a):(b))
@@ -39,5 +39,48 @@ Sigfunc*    signal_intr(int, Sigfunc*);
 
 void        daemonize(const char*);
 
-void        sleep_us(unsighed int);
-void        readn(int, void*, size_t);
+void        sleep_us(unsigned int);
+ssize_t     readn(int, void*, size_t);
+ssize_t     writen(int, void*, size_t);
+
+int         fd_pipe(int*);
+int         recv_fd(int, ssize_t (*func)(int, const void*, size_t));
+int         send_fd(int, int);
+int         send_err(int, int, const char*);
+int         serv_listen(const char*);
+int         serv_accept(int, uid_t*);
+int         cli_conn(const char*);
+int         buf_args(char*, int (*func)(int, char**));
+
+int         tty_cbreak(int);
+int         tty_raw(int);
+int         tty_reset(int);
+void        tty_atexit(void);
+struct termios* tty_termios(void);
+
+int         ptym_open(char*,int);
+int         ptys_open(char*);
+
+#ifdef TIOCGWINSZ 
+pid_t       pty_fork(int* ,char*, int, const struct termios*, const struct winsize*);
+#endif
+
+int         lock_reg(int, int, int, off_t, int, off_t);
+
+#define     read_lock(fd, offset, whence, len)\
+                lock_reg((fd), F_SETLK, F_RDLCK, (offset), (whence), (len));
+
+#define     readw_lock(fd, offset, whence, len)\
+                lock_reg((fd), F_SETLKW, F_RDLCK, (offset), (whence), (len));
+
+#define     write_lock(fd, offest, whence, len)\
+                lock_reg((fd), F_SETLK, F_WRLCK, (offset), (whence), (len));
+
+#define     writew_lock(fd, offset, whence, len)\
+                lock_reg((fd), F_SETLKW, F_WRLCK, (offset), (whence),(len));
+
+#define     un_lock(fd, offset, whence, len)\
+                lock_reg((fd), F_SETLK, F_UNLCK, (offset), (whence), (len));
+
+
+            
