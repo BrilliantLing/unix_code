4ff9c4a3de04811c501a0047c3fb857148d39eb3
BrilliantLJW <name321@qq.com>
2018-03-31 22:40:08 +0800

    update

--- include/apue.h
+++ include/apue.h
@@ -82,5 +82,33 @@ int         lock_reg(int, int, int, off_t, int, off_t);
 #define     un_lock(fd, offset, whence, len)\
                 lock_reg((fd), F_SETLK, F_UNLCK, (offset), (whence), (len));
 
+pid_t       lock_test(int, int, off_t, int, off_t);
 
-            
+#define     is_read_lockable(fd, offset, whence, len)\
+                (lock_test((fd), F_RDLCK, (offset), （whence), (len) == 0)
+
+#define     is_write_lockable(fd, offset, whence, len)\
+                (lock_test((fd), F_WRLCK, (offset), (whence), (len)) == 0)
+
+void        err_msg(const char*, ...);
+void        err_dump(const char*, ...) __attribute__((noreturn));
+void        err_quit(const char*, ...) __attribute__((noreturn));
+void        err_cont(int, const char*, ...);
+void        err_exit(int, const char*, ...) __attribute__((noreturn));
+void        err_ret(const char*, ...);
+void        err_sys(const char*, ...) __attribute__((noreturn));
+
+void        log_msg(const char*, ...);
+void        log_open(const char*, int, int);
+void        log_quit(const char*, ...) __attribute__((noreturn));
+void        log_ret(const char*, ...);
+void        log_sys(const char*, ...) __attribute__((noreturn));
+void        log_exit(int, const char*, ...) __attribute__((noreturn));
+
+void        TELL_WAIT(void);
+void        TELL_PARENT(pid_t);
+void        TELL_CHILD(pid_t);
+void        WAIT_PARENT(void);
+void        WAIT_CHILD(void);
+
+
